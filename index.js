8d02e8857983c4e1329048c81fdd5caad51ab923
ihsaanul hoque <ihsaanulhoque@gmail.com>
2022-12-04 00:10:03 +0000

    rome formatting/ sentence duplicates in textarea

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 // variables
-const sentenceTag = document.querySelector("input.checker_config-input")
-const outputTag = document.querySelector("div.checker_main-layout")
+const sentenceTag = document.querySelector("input.checker_config-input");
+const outputTag = document.querySelector("textarea.cheacker_output");
+const orginalText = outputTag.value;
 
 // event listener to copy the text from the sentence input to the H1
 sentenceTag.addEventListener("keyup", function () {
-    outputTag.innerHTML = this.value
-})
+	// if the sentence has something written in it (a value)
+	if (this.value) {
+		outputTag.innerHTML = this.value;
+	} else {
+		outputTag.innerHTML = orginalText;
+	}
+});
