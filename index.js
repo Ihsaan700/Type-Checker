76e879558289eaa3b4e707d92040fe6acb8c9137
ihsaanul hoque <ihsaanulhoque@gmail.com>
2022-12-04 19:28:59 +0000

    spelling errors and corrected value

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 // variables
 const sentenceTag = document.querySelector("input.checker_config-input");
-const outputTag = document.querySelector("textarea.cheacker_output");
-const orginalText = outputTag.value;
+const outputTag = document.querySelector("textarea.checker_output");
+const originalText = outputTag.value;
 
 // event listener to copy the text from the sentence input to the H1
 sentenceTag.addEventListener("keyup", function () {
 	// if the sentence has something written in it (a value)
 	if (this.value) {
-		outputTag.innerHTML = this.value;
+		outputTag.value = this.value;
 	} else {
-		outputTag.innerHTML = orginalText;
+		outputTag.value = originalText;
 	}
 });
 
