fceb4ac5f5afea81555fd267f9302c61a891d92e
ihsaanul hoque <ihsaanulhoque@gmail.com>
2022-12-05 10:56:08 +0000

    added font weight and line height feature

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 // variables
 const sentenceTag = document.querySelector("input.checker_config-input");
-const typeSizeTag = document.querySelector(`input[name="typesize"]`)
+
+const typeSizeTag = document.querySelector(`input[name="typeSize"]`)
 const typeSizeOutput = document.querySelector("span.font-size-output")
+
+const fontWeightTag = document.querySelector(`input[name="typeWeight"]`)
+const fontWeightOutput = document.querySelector("span.font-weight-output")
+
+const lineHeightTag = document.querySelector(`input[name="lineHeight"]`)
+const lineHeightOutput = document.querySelector("span.line-height-output")
+
 const outputTag = document.querySelector("textarea.output");
 const originalText = outputTag.value;
 
@@ -20,8 +28,20 @@ outputTag.addEventListener("keyup", function () {
 	sentenceTag.value = this.value;
 });
 
-// when font-size-range is adjusted, the font-size-output (number) adjusts accordingly
+// when font-size-range is adjusted, the font-size-output (number) adjusts accordingly along with the font size in the text area
 typeSizeTag.addEventListener("input", function () {
 		outputTag.style.fontSize = `${this.value}px`;
 		typeSizeOutput.innerHTML = `${this.value} px`;
+});
+
+// when font-weight-range is adjusted, the font-weight-output adjusts accordingly along with the font weight in the text area
+fontWeightTag.addEventListener("input", function() {
+	outputTag.style.fontWeight = this.value;
+	fontWeightOutput.innerHTML = this.value;
+});
+
+// when line-height-range is adjusted, the line-height-output adjusts accordingly along with the line height in the text area
+lineHeightTag.addEventListener("input", function () {
+	outputTag.style.lineHeight = this.value;
+	lineHeightOutput.innerHTML = this.value;
 });
